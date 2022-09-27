be9b3f4e89cf369659d750a529d575798fb2efd5
Ananya Thukral <alwaysananya@gmail.com>
2022-09-26 22:44:53 -0400

    profile screen in progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
-import Profile from './screens/ProfileScreen';
+import ProfileScreen from './screens/ProfileScreen';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import LoginScreen from './screens/LoginScreen';
-import NavBar from './components/NavBar';
 import { onAuthStateChanged } from 'firebase/auth';
-import {auth} from './firebase'
-
+import {auth} from './firebase' 
+import { useDispatch, useSelector } from 'react-redux'
+import {login, logout, selectUser} from './features/userSlice'
+ 
 function App() {
 
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(()=>{
     const unsubscribe = onAuthStateChanged(auth, async(user) => {
+      //data dispatch to the store
       if(user){
         //logged in (saves the last user logged in) 
+        dispatch(login({
+          uid: user.uid,
+          email: user.email
+        }))
       }else{
         //logged out
+        dispatch(logout)
       }
     })
     return unsubscribe
@@ -31,7 +39,7 @@ function App() {
           <LoginScreen/>
         ): (
         <Routes>
-          <Route path='/profile' element = {<Profile/>}/>
+          <Route path='/profile' element = {<ProfileScreen/>}/>
           <Route path='login' element = {<LoginScreen/>}/>
           <Route path='/' element = {<HomeScreen/>}/>
         </Routes> 
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -2,15 +2,53 @@ import React from 'react'
 import styled from 'styled-components'
 import NavBar from '../components/NavBar'
 
-const Profile = () => {
+const ProfileScreen = () => {
   return (
     <Container>
       {/* same nav styles as profile screen */}
       <NavBar screen = 'profile'/>
+      <ProfileContainer>
+        <h1>Edit Profile</h1>
+        <div className='divider'></div>
+        <Content>
+          <ProfilePicture>
+            <img src = 'https://occ-0-344-1007.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABY20DrC9-11ewwAs6nfEgb1vrORxRPP9IGmlW1WtKuaLIz8VxCx5NryzDK3_ez064IsBGdXjVUT59G5IRuFdqZlCJCneepU.png?r=229'/>
+          </ProfilePicture>
+          <ProfileDetails>
+            <EmailContainer></EmailContainer>
+            <h2>Plans (Current Plan: premimum)</h2>
+            <div className='divider'></div>
+            <h3>Renewal date: 04/03/2021</h3>
+            <Rows>
+              <Row>
+                <LeftContainer>
+                  <h4>Netflix Standard</h4>
+                  <h5>1080p</h5>
+                </LeftContainer>
+                <SubscribeButton></SubscribeButton>
+              </Row>
+              <Row>
+                <LeftContainer>
+                  <h4>Netflix Basic</h4>
+                  <h5>1080p</h5>
+                </LeftContainer>
+                <SubscribeButton></SubscribeButton>
+              </Row>
+              <Row>
+                <LeftContainer>
+                  <h4>Netflix Premium</h4>
+                  <h5>1080p</h5>
+                </LeftContainer>
+                <SubscribeButton></SubscribeButton>
+              </Row>
+            </Rows>
+          </ProfileDetails>
+        </Content>
+      </ProfileContainer>
     </Container>
   )
 }
 
-export default Profile
+export default ProfileScreen
 
 const Container = styled.div``
